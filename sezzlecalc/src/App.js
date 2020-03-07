b58e2b11e02918ea22599fd7c22b2a76082fa795
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2020-03-07 12:10:30 -0600

    first commit

---
+++ sezzlecalc/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Calculator</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
